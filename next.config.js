8ff429fec28c90a0019d56ec4d997d9ac66906a5
Chung0902 <chungnguyen.130902@gmail.com>
2024-05-29 23:42:22 +0700

    up

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/(.*)',
-        destination: '/',
-      },
-    ];
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
